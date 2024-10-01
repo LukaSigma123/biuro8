792a83eca78ea191cd310a1dd1bcdc41dfccec96
Łukasz <dzamal1835@gmail.com>
2024-10-01 21:03:14 +0200

    napisanie html

--- biuro.php
+++ biuro.php
@@ -7,6 +7,53 @@
     <link rel="stylesheet" href="styl9.css">
 </head>
 <body>
+    <header>
+        <div id="baner">
+            <h1>BIURO PODRÓŻY</h1>
+        </div>
+        
+        
+    </header>
+
+    <div id="lewy">
+        <h2>Promocje</h2>
+        <table>
+            <tr>
+               <td>Warszawa</td>
+             <td>od 600zł</td>
+          </tr>
+          <tr>
+                <td>Wenecja</td>
+                <td>od 1200zł</td>
+            </tr>
+            <tr>
+                <td>Paryż</td>
+                <td>od 1200zł</td>
+            </tr>
+        </table>
+    </div>
+
+    <div id="srodkowy">
+        <h2>W tym roku jedziemy do...</h2>
+        
+    </div>
+
+
+    <div id="prawy">
+        <h2>Kontakt</h2>
+        <a href="mailto:biuro@wycieczki.pl">napisz do nas</a>
+        <p>telefon: 444555666</p>
+    </div>
+
+    <div id="blokzdanymi">
+        <h3>W poprzednich latach bylismy...</h3>
+    </div>
+
+    <footer>
+        <p>Stronę wykonał: Łukasz Potrykus</p>
+    </footer>
+
+
 
 </body>
 </html>
