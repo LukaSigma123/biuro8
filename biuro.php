d99c18d50c83da1dc3d62565eece20b2920991b6
Łukasz <dzamal1835@gmail.com>
2024-10-01 20:49:54 +0200

    stworzenie biuro.php i styl9.css

---
+++ biuro.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Poznaj Europe</title>
+    <link rel="stylesheet" href="styl9.css">
+</head>
+<body>
+
+</body>
+</html>
